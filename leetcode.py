b2874c80eadd0ef1c0d7d523907e6c79b79af815
TasfiaAddrita <tasfia.addrita@gmail.com>
2020-04-27 06:44:52 -0700

    completed hw #7, leetcode + variable tables

--- leetcode.py
+++ leetcode.py
@@ -1,4 +1,63 @@
-'''
+# ---- HW #7, Variable Tables ---
+
+"""
+--- 1346. Check in N and Its Double Exist
+https://leetcode.com/problems/check-if-n-and-its-double-exist/
+
+Given an array arr of integers, check if there exists two integers N and M such that N is the double 
+of M ( i.e. N = 2 * M).
+
+--- VARIABLE TABLE --- 
+arr = [-20, 8, -6, -14, 0, -19, 14, 4]
+double = [-40, 16, -12, -28, 0, -38, 28, 8]
+
+num     double      in arr?     index_num       index_double        index_num != index_double?
+-------------------------------------------------------------------------------------------------------
+-20     -40         false       N/A             N/A                 N/A
+8       16          false       N/A             N/A                 N/A
+-6      -12         false       N/A             N/A                 N/A
+-14     -28         false       N/A             N/A                 N/A
+0       0           true        4               4                   false
+-19     -38         false       N/A             N/A                 N/A
+14      28          false       N/A             N/A                 N/A
+4       8           true        1               7                   true                        
+"""
+
+def checkIfExist(arr):
+    double = [num * 2 for num in arr]
+    for i, num in enumerate(arr):
+        if num in double and double.index(num) != i:
+            return True
+    return False
+
+"""
+--- 367. Valid Perfect Square ---
+https://leetcode.com/problems/valid-perfect-square/
+
+Given a positive integer num, write a function which returns True if num is a perfect square else False.
+Note: Do not use any built-in library function such as sqrt.
+
+--- VARIABLE TABLE --- 
+
+num     s           int(s)      int(s)^2 == num?
+---------------------------------------------------
+16      4           4           true
+14      3.74...     3           false
+36      6           6           true
+256     16          16          true
+254     15.93...    15          false
+
+--- Ways to make this better --- 
+Odds are the interviewer would prefer to see a solution that doesn't involve the exponent operation.
+"""
+
+def isPerfectSquare(num):
+    s = num ** (1/2)
+    return int(s) ** 2 == num
+
+# ---- HW #2, Communication Steps ---
+
+"""
 --- 122. Best Time to Buy and Sell Stock II --- 
 https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
 
@@ -23,7 +82,7 @@ I drew out the prices array in a notebook and looked for any patterns.
 
 As I was calculating the differences for each day, I noticed that you only want to buy when you reach a low number and 
 sell when you're on a high number. 
-'''
+"""
 
 def max_profit(prices):
     profit = 0
@@ -43,7 +102,7 @@ def max_profit(prices):
 
 print(max_profit(lst))
 
-'''
+"""
 --- 4. Median of Two Sorted Arrays ---
 https://leetcode.com/problems/median-of-two-sorted-arrays/
 
@@ -71,7 +130,7 @@ This is a decent solution since it runs O(log(m+n)), but I wonder if I built a n
 would be more efficient because I can end the sorting when we reach the median index (so we don't have to 
 traverse through all of the elements in both arrays). But this requires a lot of conditionals and keeping 
 track of values which can risk the code breaking.
-'''
+"""
 
 def findMedianSortedArrays(nums1, nums2):
     nums1.extend(nums2)
